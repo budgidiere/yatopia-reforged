93bad30a096432391b49ac5b917d0094a45ac997
Zoe <duplexsys@protonmail.com>
2020-12-26 19:50:33 -0600

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,50 @@
+plugins {
+    `java-library`
+    toothpick
+}
+
+toothpick {
+    forkName = "Yatopia"
+    groupId = "org.yatopiamc"
+    val versionTag = System.getenv("BUILD_NUMBER")
+        ?: "\"${gitCmd("rev-parse", "--short", "HEAD").output}\""
+    forkVersion = "git-$forkName-$versionTag"
+    forkUrl = "https://github.com/YatopiaMC/Yatopia"
+
+    minecraftVersion = "1.16.4"
+    nmsPackage = "1_16_R3"
+    nmsRevision = "R0.1-SNAPSHOT"
+
+    upstream = "paper"
+    upstreamBranch = "origin/master"
+
+    paperclipName = "Yatopia-launcher.jar"
+
+    server {
+        project = project(":$forkNameLowercase-server")
+        patchesDir = rootProject.projectDir.resolve("patches/server")
+    }
+    api {
+        project = project(":$forkNameLowercase-api")
+        patchesDir = rootProject.projectDir.resolve("patches/api")
+    }
+}
+
+subprojects {
+    repositories {
+        mavenCentral()
+        maven("https://repo.aikar.co/content/groups/aikar/")
+        maven("https://nexus.velocitypowered.com/repository/velocity-artifacts-snapshots/")
+        maven("https://libraries.minecraft.net")
+        mavenLocal()
+    }
+
+    java {
+        if(JavaVersion.VERSION_1_8 > JavaVersion.current()){
+            error("This build must be run with java 8")
+        }
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.current()
+        withSourcesJar()
+    }
+}
